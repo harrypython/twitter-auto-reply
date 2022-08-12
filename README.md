b7088c9a750933ffe2f666ceb999d45daa1ea967
Harry Python <harrypython@protonmail.com>
2022-08-12 11:11:02 -0400

    replace command line arguments to json config file

--- README.md
+++ README.md
@@ -12,20 +12,27 @@ cd twitter-auto-reply
 pip install -r requirements.txt
 ```  
 
+## Config file: config.json
+
+```json
+{
+   "username":"your-twitter-username",
+   "password":"your-twitter-password",
+   "otp":"your-one-time-password-if-you-use",
+   "search":"the message you are searching",
+   "message":"the reply message, can be a link",
+   "min_replies":0,
+   "min_faves":0,
+   "min_retweets":0,
+   "headless":0,
+   "exclude_word":"word-that-will-not-be-show-in-the-results"
+} 
+```
+
 ## Usage
 
 ```bash
-python twitter-auto-reply.py 
-                --username "your-twitter-username" 
-                --password "your-twitter-password" 
-                --otp "your-one-time-password-if-you-use" 
-                --search "the message you are searching" 
-                --message "the reply message, can be a link" 
-                --min_replies 0 
-                --min_faves 0 
-                --min_retweets 0
-                --headless / --no-headless #browser headless
-                --exclude_word "word-that-will-not-be-show-in-the-results"
+python twitter-auto-reply.py --config my-config-file.json
 ```
 
 ## Contributing
--- twitter-auto-reply.py
+++ twitter-auto-reply.py
@@ -1,3 +1,5 @@
+import json
+import argparse
 import selenium.common.exceptions
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options as FirefoxOptions
@@ -7,7 +9,6 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as exp_cond
 import pyotp as pyotp
-import argparse
 from loguru import logger
 
 
@@ -17,25 +18,16 @@ def get_otp(k):
 
 
 def search_and_reply():
-    parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    parser.add_argument('--headless', dest='headless', action='store_true', help="Headless Browser")
-    parser.add_argument('--no-headless', dest='headless', action='store_false', help="Headless Browser")
-    parser.set_defaults(headless=True)
-    parser.add_argument("-u", "--username", type=str, help="Twitter Username")
-    parser.add_argument("-p", "--password", type=str, help="Twitter Password")
-    parser.add_argument("-otp", "--otp", type=str, help="OTP Secret Key")
-    parser.add_argument("-s", "--search", type=str, help="String to search")
-    parser.add_argument("-m", "--message", type=str, help="Reply message")
-    parser.add_argument("-mr", "--min_replies", type=int, default=0, help="Minimal replies")
-    parser.add_argument("-mf", "--min_faves", type=int, default=0, help="Minimal favorites")
-    parser.add_argument("-mrt", "--min_retweets", type=int, default=0, help="Minimal retweets")
-    parser.add_argument("-e", "--exclude_word", type=str, help="Exclude word")
-
-    args = parser.parse_args()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--config', help='Load settings from file in json format.')
+    conf_args = parser.parse_args()
+    if conf_args.config:
+        with open(conf_args.config, "r") as fp:
+            args = json.load(fp)
 
     try:
         options = FirefoxOptions()
-        if args.headless:
+        if bool(args['headless']):
             options.add_argument("--headless")
             logger.info("Running headless browser")
         s = Service(GeckoDriverManager().install())
@@ -49,35 +41,37 @@ def search_and_reply():
         quit()
 
     try:
-        wait.until(exp_cond.visibility_of_element_located((By.XPATH, "//input[@name='text']"))).send_keys(args.username)
+        wait.until(
+            exp_cond.visibility_of_element_located((By.XPATH, "//input[@name='text']"))
+        ).send_keys(args['username'])
         wait.until(exp_cond.visibility_of_element_located((By.XPATH, "//span[text()='Next']"))).click()
         wait.until(
             exp_cond.visibility_of_element_located((By.XPATH, "//input[@name='password']"))
-        ).send_keys(args.password)
+        ).send_keys(args['password'])
         wait.until(exp_cond.visibility_of_element_located((By.XPATH, "//span[text()='Log in']"))).click()
 
-        if args.otp is not None:
+        if bool(args['otp']):
             wait.until(
                 exp_cond.visibility_of_element_located((By.XPATH, "//input[@name='text']"))
-            ).send_keys(get_otp(args.otp))
+            ).send_keys(get_otp(args['otp']))
             wait.until(exp_cond.visibility_of_element_located((By.XPATH, "//span[text()='Next']"))).click()
 
         if bool(wait.until(exp_cond.visibility_of_element_located((By.XPATH, "//span[text()='Home']")))):
             logger.info("Successfully logged in")
-    except selenium.common.exceptions.TimeoutException as to:
+    except selenium.common.exceptions.TimeoutException:
         logger.error("Login error! Sorry!")
         driver.close()
         quit()
 
     q = '{} min_replies:{} min_faves:{} min_retweets:{} -filter:links -filter:replies'.format(
-        args.search,
-        args.min_replies,
-        args.min_faves,
-        args.min_retweets
+        args['search'],
+        args['min_replies'],
+        args['min_faves'],
+        args['min_retweets']
     )
 
-    if args.exclude_word is not None:
-        q = q + " -{}".format(args.exclude_word)
+    if bool(args['exclude_word']):
+        q = q + " -{}".format(args['exclude_word'])
 
     logger.info("Doing search")
     try:
@@ -87,15 +81,15 @@ def search_and_reply():
         wait.until(exp_cond.visibility_of_element_located((By.XPATH, "//div[@contenteditable='true']"))).send_keys(" ")
         wait.until(
             exp_cond.visibility_of_element_located((By.XPATH, "//div[@contenteditable='true']"))
-        ).send_keys(args.message)
-    except selenium.common.exceptions.TimeoutException as to:
+        ).send_keys(args['message'])
+    except selenium.common.exceptions.TimeoutException:
         logger.error("Search error! Sorry!")
         driver.close()
         quit()
 
     try:
         wait.until(exp_cond.visibility_of_element_located((By.XPATH, "//div[@data-testid='tweetButton']"))).click()
-    except selenium.common.exceptions.TimeoutException as to:
+    except selenium.common.exceptions.TimeoutException:
         logger.error("Reply error! Sorry!")
         driver.close()
         quit()
@@ -108,3 +102,4 @@ def search_and_reply():
 
 if __name__ == '__main__':
     search_and_reply()
+    quit()
